2d6980e56b6efd98e514902cc6ef74a20564df73
Dmitry P <dimong@inbox.lv>
2022-04-07 19:39:23 +0200

    Add Output Arrays

--- Program.cs
+++ Program.cs
@@ -12,5 +12,28 @@ void PrintArray(string[] array)
 }
 
 string[] array = {"Hello", "2", "world", ":-)"};
-string[] newarray (string[] array);
+string[] newarray (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
 
+    string[] newarray = new string [count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[j] = array[i];
+            j++;
+        }
+    }
+    return newarray;
+}
+
+PrintArray(array);
+PrintArray(newarray(array));
